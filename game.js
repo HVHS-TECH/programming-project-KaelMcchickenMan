68458d7761744383051c377c6aa5df88ce22ce34
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-03-03 15:14:43 +1300

    Updated Gameplay Yippie

--- game.js
+++ game.js
@@ -4,7 +4,7 @@
 // GLOBAL 
 
 	let wallGroupVelocity = 1;
-
+speedingUpWalls();
 
 //SETUP
 function setup() {
@@ -55,11 +55,63 @@ function setup() {
 
 		function rightChunk() {
 			console.log("rightChunk");
-			wall = new Sprite(375, 0, 200, 20, 'k');
+			wall = new Sprite(375, 0, 150, 20, 'k');
 			wall.color = 'rgb(116, 100, 86)';
 			wall.vel.y = wallGroupVelocity;
 			wallGroup.add(wall);
-			wall = new Sprite(375, 100, 20,300, 'k');
+			wall = new Sprite(300, 25, 20,200, 'k');
+			wall.color = 'rgb(116, 100, 86)';
+			wall.vel.y = wallGroupVelocity;
+			wallGroup.add(wall);
+			wall = new Sprite(76, -75, 468, 20, 'k');
+			wall.color = 'rgb(116, 100, 86)';
+			wall.vel.y = wallGroupVelocity;
+			wallGroup.add(wall);
+		}
+
+		async function waveLarge() {
+			console.log("waveLarge");
+		 	 leftSideWave();
+			await sleep(6000);
+			rightSideWave();
+			await sleep(6000);
+			 leftSideWave();
+			await sleep(5500);
+			rightSideWave();
+			await sleep(5500);
+			 leftSideWave();
+			await sleep(5000);
+			rightSideWave();
+			await sleep(5000);
+				wallGroupVelocity = wallGroupVelocity + wallGroupVelocity * 0.5
+		}
+
+
+		function rightSideWave() {
+			wall = new Sprite(200, -75, 20,200, 'k');
+			wall.color = 'rgb(116, 100, 86)';
+			wall.vel.y = wallGroupVelocity;
+			wallGroup.add(wall);
+			wall = new Sprite(424, -175, 468, 20, 'k');
+			wall.color = 'rgb(116, 100, 86)';
+			wall.vel.y = wallGroupVelocity;
+			wallGroup.add(wall);
+			wall = new Sprite(424, 25, 468, 20, 'k');
+			wall.color = 'rgb(116, 100, 86)';
+			wall.vel.y = wallGroupVelocity;
+			wallGroup.add(wall);
+		}
+
+		function leftSideWave() {
+				wall = new Sprite(300, -75, 20,200, 'k');
+			wall.color = 'rgb(116, 100, 86)';
+			wall.vel.y = wallGroupVelocity;
+			wallGroup.add(wall);
+			wall = new Sprite(76, -175, 468, 20, 'k');
+			wall.color = 'rgb(116, 100, 86)';
+			wall.vel.y = wallGroupVelocity;
+			wallGroup.add(wall);
+			wall = new Sprite(76, 25, 468, 20, 'k');
 			wall.color = 'rgb(116, 100, 86)';
 			wall.vel.y = wallGroupVelocity;
 			wallGroup.add(wall);
@@ -68,17 +120,20 @@ function setup() {
 		wallSpawning();
 		//gameplay spawning
 		async function wallSpawning() {
-  leftBlockage();
-  await sleep(2000);
-  rightBlockage();
-   await sleep(2000);
-   leftBlockage();
-    await sleep(2000);
-   rightChunk();
-    await sleep(7000);
-    rightBlockage();
-   await sleep(2000);
-   leftBlockage();
+ // leftBlockage();
+  //await sleep(5000);
+   waveLarge();
+   await sleep(40000);
+   waveLarge();
+   await sleep(40000);
+  // leftBlockage();
+   // await sleep(2000);
+  // rightChunk();
+   // await sleep(7000);
+    //rightBlockage();
+  // await sleep(2000);
+   //leftBlockage();
+   
 }
 		
 		
@@ -94,8 +149,10 @@ function draw() {
 	background('rgb(153, 0, 0)');
 	startGame();
 
-	wallGroupVelocity = wallGroupVelocity + wallGroupVelocity * 0.05 / 50
+	
 
+	
+	
 	function startGame() {
 		player.vel.x = player.vel.x / 1.05;
 		player.vel.y = player.vel.y / 1.05;
